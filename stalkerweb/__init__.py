c9b4f4c456f6a1f308c637847134b598f9d4e129
Florian Hines <syn@ronin.io>
2013-04-11 23:26:43 -0700

    Support stalkerweb configs
    - Load config via env var or stalker-web option
    - Include sample stalkerweb.cfg
    - Add bin/stalker-web flag to generate a config

--- stalkerweb/__init__.py
+++ stalkerweb/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask.ext.pymongo import PyMongo
 
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__, instance_relative_config=False)
 app.config['MONGO_DBNAME'] = 'stalkerweb'
+app.config['MONGO_USERNAME'] = None
+app.config['MONGO_PASSWORD'] = None
 app.config['REGISTER_KEY'] = 'itsamario'
 app.config['API_KEY'] = 'something'
 app.config['SECRET_KEY'] = 'SuperSecretDevKeyChangeMe!'
 app.config['THEMES'] = ['cosmo', 'cerulean', 'cyborg', 'slate', 'spacelab', 'united']
+app.config.from_envvar('STALKERWEB_CONFIG')
 mongo = PyMongo(app)
 
 from stalkerweb import views
